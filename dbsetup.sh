bfe07ec4ace26cbce4d01b9235cdb7bf47201095
Brett Longworth <blongworth@gmail.com>
2016-03-04 22:52:06 -0500

    create sqlite3 db, write data to db

---
+++ dbsetup.sh
@@ -0,0 +1,5 @@
+CREATE TABLE temp (
+ts int primary key,
+temp int,
+rh int
+);
--- labtemp.sh
+++ labtemp.sh
@@ -2,8 +2,11 @@
 
 # Read temp and RH from Omega iTHX
 
+db=$'/home/brett/Projects/labtemp/labtemp.db'
 ts=$(date +%s)
 temp=$(printf "*SRTF\r" | nc thdcfams 2000)
 sleep .5
 rh=$(printf "*SRH\r" | nc thdcfams 2000)
 printf "%s,%s,%s\n" "$ts" "$temp" "$rh"
+
+echo 'insert into temp (ts, temp, rh) values ('\'''$ts''\'', '\'''$temp''\'', '$rh');'|sqlite3 $db
