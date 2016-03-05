693eed32c843097a6daa4963ca79e4464e1cb3ed
Brett Longworth <blongworth@gmail.com>
2016-03-05 15:13:25 -0500

    Code cleanup

--- app.R
+++ app.R
@@ -9,30 +9,21 @@ server <- function(input, output) {
   tempdata <- reactive({
     data <- read.csv(file, col.names=c("ts","temp","rh"))
     data$ts <- as.POSIXct(data$ts, origin="1970-01-01")
-    # Due to dplyr issue #318, we need temp variables for input values
-    mindate <- input$date[1]
-    maxdate <- input$date[2]
     
     # Apply filters
     m <- data %>%
       filter(
-        as.Date(ts) >= as.Date(mindate),
-        as.Date(ts) <= as.Date(maxdate)
+        as.Date(ts) >= as.Date(input$date[1]),
+        as.Date(ts) <= as.Date(input$date[2])
       ) 
     
     as.data.frame(m)
     
   }) 
   
-  #output$st <- tempdata()[1,1]
-  
   output$tempPlotly <- renderPlotly({
     p <- plot_ly(tempdata(), x = ts, y = temp)
     p
-    #p %>%
-    #  add_trace(y = fitted(loess(temp ~ ts))) %>%
-    #  layout(title = "NOSAMS lab temperature",
-    #         showlegend = FALSE) 
   })
   output$rhPlotly <- renderPlotly({
     p <- plot_ly(tempdata(), x = ts, y = rh)
@@ -47,13 +38,9 @@ ui <- fluidPage(
     sidebarPanel(
       dateRangeInput('date',
                      label = 'Date Range',
-                     #start = as.Date(output$st), 
                      start = (Sys.Date() - 5),
                      end = Sys.Date(),
                      max = Sys.Date())
-#      sliderInput('smooth',
-#                  label = 'Smoothing',
-#                  0, 10, value = 1)
     ),
     mainPanel(plotlyOutput("tempPlotly"),
               plotlyOutput("rhPlotly"))
