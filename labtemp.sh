b3a4344a8046d03dbcafe4f79e0a56f70dc57222
Brett Longworth <blongworth@gmail.com>
2019-01-31 13:11:21 -0500

    add second try after timeout

--- labtemp.sh
+++ labtemp.sh
@@ -4,15 +4,22 @@
 
 db=$'/home/brett/Projects/labtemp/labtemp.db'
 ts=$(date +%s)
-temp=$(printf "*SRTF\r" | nc -w 1 thdcfams 2000)
+temp=$(printf "*SRTF\r" | nc -w 2 thdcfams 2000)
 if [ "$?" = "1" ]; then 
-	temp="0"
+  temp=$(printf "*SRTF\r" | nc -w 2 thdcfams 2000)
+  if [ "$?" = "1" ]; then 
+    temp="0"
+  fi
 fi
 
-sleep .5
-rh=$(printf "*SRH\r" | nc -w 1 thdcfams 2000)
+sleep 1
+
+rh=$(printf "*SRH\r" | nc -w 2 thdcfams 2000)
 if [ "$?" = "1" ]; then 
-	rh="0"
+  rh=$(printf "*SRH\r" | nc -w 2 thdcfams 2000)
+  if [ "$?" = "1" ]; then 
+    rh="0"
+  fi
 fi
 
 #printf "%s,%s,%s\n" "$ts" "$temp" "$rh"
